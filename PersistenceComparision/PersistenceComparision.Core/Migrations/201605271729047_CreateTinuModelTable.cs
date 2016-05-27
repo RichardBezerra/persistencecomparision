689f4a487933afafd54676513e5eb685dc6403b7
Richard Bezerra <richardbezerra@gmail.com>
2016-05-27 14:31:18 -0300

    First version of databse, including only tinymodel table, working.

---
+++ PersistenceComparision/PersistenceComparision.Core/Migrations/201605271729047_CreateTinuModelTable.cs
@@ -0,0 +1,26 @@
+namespace PersistenceComparision.Core.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTinuModelTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.TinyModel",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Descricao = c.String(unicode: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.TinyModel");
+        }
+    }
+}
