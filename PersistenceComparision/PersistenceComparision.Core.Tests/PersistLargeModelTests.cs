216a97c5040cbfdc89f45a982f61c8cdb388989b
Richard Bezerra <richardbezerra@gmail.com>
2016-06-03 21:49:04 -0300

    Batch persistence of the large model

---
+++ PersistenceComparision/PersistenceComparision.Core.Tests/PersistLargeModelTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace PersistenceComparision.Core.Tests
+{
+    [TestFixture]
+    public class PersistLargeModelTests : AssertionHelper
+    {
+        [Test, Combinatorial]
+        public void Create_sequentialy_N_large_entities([Values(100)] int qtd, [Values("ADO", "EF", "ORMLite")] string impl)
+        {
+            var testImpls = new RepoImplsTests();
+
+            for (int i = 0; i < qtd; i++)
+                testImpls.Create_LargeModel(impl);
+        }
+
+        [Test, Combinatorial]
+        public void CRUD_sequentialy_N_large_entities([Values(100)] int qtd, [Values("ADO", "EF", "ORMLite")] string impl)
+        {
+            var testImpls = new RepoImplsTests();
+
+            for (int i = 0; i < qtd; i++)
+                testImpls.CRUD_LargeModel(impl);
+        }
+
+        [Test, Combinatorial]
+        public void Create_parallely_N_large_entities([Values(100)] int qtd, [Values("ADO", "EF", "ORMLite")] string impl)
+        {
+            Parallel.For(0, qtd, (int i) =>
+            {
+                var testImpls = new RepoImplsTests();
+
+                testImpls.Create_LargeModel(impl);
+            });
+        }
+
+        [Test, Combinatorial]
+        public void CRUD_parallely_N_large_entities([Values(100)] int qtd, [Values("ADO", "EF", "ORMLite")] string impl)
+        {
+            Parallel.For(0, qtd, (int i) =>
+            {
+                var testImpls = new RepoImplsTests();
+
+                testImpls.CRUD_LargeModel(impl);
+            });
+        }
+    }
+}
